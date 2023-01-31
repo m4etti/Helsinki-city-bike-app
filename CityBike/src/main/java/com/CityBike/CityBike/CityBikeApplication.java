9cf8a7d4af76af89e65fd76dfaacbb2c428795bd
m4etti <matti.vout@gmail.com>
2023-02-01 01:43:05 +0200

    Spring Boot boiler plate

---
+++ CityBike/src/main/java/com/CityBike/CityBike/CityBikeApplication.java
@@ -0,0 +1,13 @@
+package com.CityBike.CityBike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityBikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityBikeApplication.class, args);
+	}
+
+}
---
+++ CityBike/src/test/java/com/CityBike/CityBike/CityBikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CityBike.CityBike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityBikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
